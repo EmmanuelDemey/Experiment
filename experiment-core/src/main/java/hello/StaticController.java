b23b1a82a9be766d49161cf5a146f825ffab65c1
Emmanuel DEMEY <emmanuel.demey@atos.net>
2014-05-04 14:17:23 +0200

    Add Static files support

---
+++ experiment-core/src/main/java/hello/StaticController.java
@@ -0,0 +1,15 @@
+package hello;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class StaticController {
+
+    @RequestMapping({
+	    "/admin"
+	})
+	public String index() {
+	    return "forward:/index.html";
+	}
+}
